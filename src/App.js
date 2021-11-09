d23a486f63aac6b6293989b6c45f73ebe7d46e61
tyler <tylertooxclusive@gmail.com>
2021-11-09 22:35:32 +0000

    restructuring

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Hero } from './components/hero/Hero';
+import './assets/css/main/styles.css'
+
+function App() {
+  return (
+    <div>
+        <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Social.jsx
@@ -0,0 +1,23 @@
+import * as FaIcons from 'react-icons/fa'
+
+export const Social = () => {
+    return (
+        <div className='social'>
+            <div>
+                <FaIcons.FaGithub />
+            </div>
+            <div>
+                <FaIcons.FaTwitter />
+            </div>
+            <div>
+                <FaIcons.FaYoutube />
+            </div>
+            <div>
+                <FaIcons.FaDribbble />
+            </div>
+            <div>
+                <FaIcons.FaLinkedinIn />
+            </div>
+        </div>
+    )
+}
